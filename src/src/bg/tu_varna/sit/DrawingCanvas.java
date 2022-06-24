d0ca128f23622d3c809fa3978f6ed8f6b2632c0f
StoyanOrtsev <35624797+StoyanOrtsev@users.noreply.github.com>
2022-06-25 00:21:27 +0300

    Updates on the print method

--- src/src/bg/tu_varna/sit/DrawingCanvas.java
+++ src/src/bg/tu_varna/sit/DrawingCanvas.java
@@ -2,23 +2,56 @@ package bg.tu_varna.sit;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DrawingCanvas extends JComponent {
 
     private int width;
     private int height;
-
-    private static final ArrayList<Figure> shapes = new ArrayList<>();
+    private final java.util.List<Figure> shapes;
+    private final List<Figure> result;
 
     private Rectangle r1;
     private Circle circle1;
     private Line l1;
 
+
+    private String shapeType;//withing
+    private int shapeNumber;//translate
+    private boolean translateAll;//translate
+
+    public String getShapeType() {
+        return shapeType;
+    }
+    public int getShapeNumber() {
+        return shapeNumber;
+    }
+    public boolean isTranslateAll() {
+        return translateAll;
+    }
+
+    public void setShapeType(String shapeType) {
+        this.shapeType = shapeType;
+    }
+
+    public void setShapeNumber(int shapeNumber) {
+        this.shapeNumber = shapeNumber;
+    }
+
+    public void setTranslateAll(boolean translateAll) {
+        this.translateAll = translateAll;
+    }
+
     public DrawingCanvas(int w, int h)
     {
         width = w;
         height = h;
+
+        shapes = new ArrayList<>();
+        result = new ArrayList<>();
+
         r1 = new Rectangle("orange",50,75,100,250);//R,G,B(RGB channels) - RED|GREEN|BLUE 0-255
         shapes.add(r1);
         circle1 = new Circle("blue",200,75,100);
@@ -33,9 +66,61 @@ public class DrawingCanvas extends JComponent {
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        //background
+        Rectangle background = new Rectangle("black", 0, 0, width, height);
+        background.draw(g2d);
+        AffineTransform reset = g2d.getTransform();//for resetting the translation
+
+        //translate
+        if (translateAll == true) {
+            g2d.translate(100, 150);//newX,newY
+            for (Figure current : shapes)
+                current.draw(g2d);
+            g2d.setTransform(reset);//-deleting the changes from the translation
+
+        } else {
+            if (shapeNumber != 0) {
+                for (Figure current : shapes)
+                    if (current == shapes.get(shapeNumber - 1)) {
+                        g2d.translate(150, 100);
+                        shapes.get(shapeNumber - 1).draw(g2d);
+                        g2d.setTransform(reset);
+                    } else current.draw(g2d);
+            }
+        }
+
+        //within
+        if (shapeType != null) {
+            switch (shapeType) {
+                case "circle":
+                    for (Figure current : shapes)
+                        if (current instanceof Circle)
+                            result.add(current);
+
+                    break;
+                case "rect":
+                    for (Figure current : shapes)
+                        if (current instanceof Rectangle)
+                            result.add(current);
+                    break;
+                case "line":
+                    for (Figure current : shapes)
+                        if (current instanceof Line)
+                            result.add(current);
+                    break;
+            }
+            for (Figure current : result)
+                current.draw(g2d);
+            result.clear();
+        }
 
         //print arrayList
-        for(Figure current : shapes)
-            current.draw(g2d);
+        if(translateAll == false && shapeNumber == 0 && shapeType == null)
+            for(Figure current : shapes)
+                current.draw(g2d);
+
+        setTranslateAll(false);
+        setShapeNumber(0);
+        setShapeType(null);
     }
 }
--- src/src/bg/tu_varna/sit/Main.java
+++ src/src/bg/tu_varna/sit/Main.java
@@ -1,7 +1,48 @@
 package bg.tu_varna.sit;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        //-- part of the print method
+        //how to run the project to the command prompt - ~/Desktop/java-graphics $ javac *.java
+        //~/Desktop/java-graphics $ javac Main /DrawingTester
+        //JFrame - it can be in separated class
+        int w = 1280;
+        int h = 720;
+        JFrame f = new JFrame();
+
+        DrawingCanvas drawingCanvas = new DrawingCanvas(w,h);
+
+        f.setSize(w,h);
+        f.setTitle("Drawing in Java");
+
+        f.add(drawingCanvas);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+
+        //--
+
+//                try {
+//                    //prints all from add(dc)
+//
+//                    //pause for 5 seconds
+//
+//                    TimeUnit.SECONDS.sleep(3);
+//                    drawingCanvas.setShapeNumber(1);
+//                    drawingCanvas.print();
+//
+//                    TimeUnit.SECONDS.sleep(3);
+//                    drawingCanvas.setTranslateAll(true);
+//                    drawingCanvas.print();
+//
+//                    TimeUnit.SECONDS.sleep(3);
+//                    drawingCanvas.setShapeType("rect");
+//                    drawingCanvas.print();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
 
     }
 }
