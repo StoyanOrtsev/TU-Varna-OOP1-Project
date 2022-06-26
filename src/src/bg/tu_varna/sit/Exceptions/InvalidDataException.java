c23efeb898e3b9e46e651796c7e059ad198d6a57
StoyanOrtsev <35624797+StoyanOrtsev@users.noreply.github.com>
2022-06-26 22:11:36 +0300

    Delete InvalidDataException.java

--- src/src/bg/tu_varna/sit/Exceptions/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.Exceptions;
-
-public class InvalidDataException extends Exception {
-    public InvalidDataException(String errorMessage) {
-        super(errorMessage);
-    }
-}
