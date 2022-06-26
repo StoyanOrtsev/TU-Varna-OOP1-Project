8ec52123c929a2040ba9cfe459591a5df1b9b07f
StoyanOrtsev <35624797+StoyanOrtsev@users.noreply.github.com>
2022-06-26 22:29:07 +0300

    Update on main menu

--- src/src/bg/tu_varna/sit/Main.java
+++ src/src/bg/tu_varna/sit/Main.java
@@ -6,14 +6,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException{
-        //-- part of the print method
-        //how to run the project to the command prompt - ~/Desktop/java-graphics $ javac *.java
-        //~/Desktop/java-graphics $ javac Main /DrawingTester
-        //JFrame - it can be in separated class
 
         JFrame myFrame = new JFrame();
-
         DrawingCanvas tempDC = new DrawingCanvas();
+
         DrawingCanvas drawingCanvas = tempDC;
 
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,23 +24,55 @@ public class Main {
             FileEditor operation = new FileEditor();
 
             System.out.println("\nWelcome to the drawing canvas!\nDrawing Canvas -> ");
-            Scanner input = new Scanner(System.in);
-            String line = input.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
 
 
-            while (true)
+            do
             {
-                String[] argsArr = line.split(" ");
-                String command = argsArr[0];
+                String[] input = line.split(" ");
+                String command = input[0];
 
                 switch (command)
                 {
+                    //shape methods: print, create, erase <n>, translate <n>, within <option>
+                    case "print":
+                        myFrame.repaint();
+                        break;
+                    case "create":
+                        String shapeType, color;
+                        double xStart, yStart, thirdValue, fourthValue;
+                        shapeType = input[1];
+                        color = input[2];
+                        xStart = Double.parseDouble(input[3]);
+                        yStart = Double.parseDouble(input[4]);
+                        thirdValue = Double.parseDouble(input[5]);
+                        fourthValue = Double.parseDouble(input[6]);
+                        drawingCanvas.createShape(shapeType,color,xStart,yStart,thirdValue,fourthValue);
+                        break;
+                    case "erase":
+                        drawingCanvas.deleteShape(Integer.parseInt(input[1]));
+                        break;
+                    case "translate":
+                        if(input[1].equals("all"))
+                            drawingCanvas.setTranslateAll(true);
+                        else
+                            drawingCanvas.setShapeNumber(Integer.parseInt(input[1]));
+
+                        myFrame.repaint();
+
+                        break;
+                    case "within":
+                        drawingCanvas.setShapeType((String)input[1]);
+                        myFrame.repaint();
+                        break;
+                    //file methods: open, close, exit, save, save as, help
                     case "open":
                         myFrame.remove(drawingCanvas);
-                        drawingCanvas = operation.open(argsArr[1]);
+                        drawingCanvas = operation.open(input[1]);
                         myFrame.add(drawingCanvas);
                         myFrame.repaint();
-                        System.out.println("Successfully opened " + argsArr[1]);
+                        System.out.println("Successfully opened " + input[1]);
                         break;
                     case "close":
                         System.out.println(operation.close());
@@ -61,18 +89,20 @@ public class Main {
                         System.out.println(operation.save(drawingCanvas));
                         break;
                     case "saveas":
-                        System.out.println(operation.saveAs(argsArr[1], argsArr[2], drawingCanvas));
+                        System.out.println(operation.saveAs(input[1], input[2], drawingCanvas));
                         break;
                     case "help":
                         System.out.println(
-                                "\tShape commands:\n" +
+                                "\n\tShape commands:\n" +
                                         "print           show all shapes\n" +
                                         "create          create a shape\n" +
+                                        "Color choices:\n" +
+                                        "blue/cyan/dark_gray/gray/green/light_gray/magenta/orange/pink/red/white/yellow\\n" +
                                         "erase <n>       delete a shape with number <n>\n" +
                                         "translate [<n>] translate a shape with number <n> or translate all shapes if <n> is not selected\n" +
                                         "within <option> show all shapes from a specific shape type selected with <option>\n" +
 
-                                        "\n\tFile commands:\n" +
+                                "\n\tFile commands:\n" +
                                         "open <file>     opens <file>\n" +
                                         "close           closes currently opened file\n" +
                                         "save            saves the currently open file\n" +
@@ -82,69 +112,14 @@ public class Main {
 
                         );
                         break;
-                    //case - function: print, create, erase <n>, translate <n>, within <option>
-                    case "print":
-                        //...
-                        myFrame.repaint();
-                        break;
-                    case "create":
-                        //...
-                        double xStart,yStart,width,height,size,xEnd,yEnd;
-                        String color;
-                        switch (argsArr[1])
-                        {
-                            case "rect":
-                                color = argsArr[2];
-                                xStart = Double.parseDouble(argsArr[3]);
-                                yStart = Double.parseDouble(argsArr[4]);
-                                width = Double.parseDouble(argsArr[5]);
-                                height = Double.parseDouble(argsArr[6]);
-                                drawingCanvas.createRect(color, xStart, yStart, width, height);
-                                break;
-                            case "circle":
-                                color = argsArr[2];
-                                xStart = Double.parseDouble(argsArr[3]);
-                                yStart = Double.parseDouble(argsArr[4]);
-                                size = Double.parseDouble(argsArr[5]);
-                                drawingCanvas.createCircle(color, xStart, yStart, size);
-                                break;
-                            case "line":
-                                color = argsArr[2];
-                                xStart = Double.parseDouble(argsArr[3]);
-                                yStart = Double.parseDouble(argsArr[4]);
-                                xEnd = Double.parseDouble(argsArr[5]);
-                                yEnd = Double.parseDouble(argsArr[6]);
-                                drawingCanvas.createLine(color, xStart, yStart, xEnd, yEnd);
-                                break;
-                        }
-                        break;
-                    case "erase":
-                        //...
-                        drawingCanvas.deleteShape(Integer.parseInt(argsArr[1]));
-                        break;
-                    case "translate":
-                        //...
-                        if(argsArr[1].equals("all"))
-                            drawingCanvas.setTranslateAll(true);
-                        else
-                            drawingCanvas.setShapeNumber(Integer.parseInt(argsArr[1]));
-
-                        myFrame.repaint();
-
-                        break;
-                    case "within":
-                        //...
-                        drawingCanvas.setShapeType((String)argsArr[1]);
-                        myFrame.repaint();
-                        break;
                     default:
                         System.out.println("Incorrect command! Try again!");
                 }
 
                 System.out.print("\nDrawing Canvas -> ");
-                line = input.nextLine();
+                line = scanner.nextLine();
                 Thread.sleep(0);
-            }
+            }while (true);
         }
         catch (InterruptedException exception)
         {
